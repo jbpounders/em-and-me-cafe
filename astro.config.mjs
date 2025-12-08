f8aa1592eb8f3dd5747ecd5ad0f2cdb17d6d4df6
jbpounders <jbpounders+github@gmail.com>
2025-12-07 22:01:19 -0400

    Initial commit - Astro frontend for Em & Me Cafe

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import vercel from '@astrojs/vercel';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel()
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  })
 });
